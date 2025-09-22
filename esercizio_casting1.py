f8338f19dda43323db53595b1255dbb344297588
Massimo <137682617+MassimoPelli@users.noreply.github.com>
2025-09-22 17:24:28 +0200

    Add files via upload

---
+++ esercizio_casting1.py
@@ -0,0 +1,7 @@
+numero = int(input("insierisci un numero intero"))
+
+numero_float = float(numero)
+print("numero convertito in float", numero_float)
+
+numero_str = str(numero)
+print(numero_str, "questo numero è stato convertito in stringa")
